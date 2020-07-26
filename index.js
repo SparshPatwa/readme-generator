4767de5f953a3f7139eab8b59317d25b57045b81
Sparsh Patwa <patwa.sparsh@gmail.com>
2020-07-26 14:05:54 -0700

    Updated index.js to use inquirer to gather CLIs and push to README.md using generateMarkdown.

--- index.js
+++ index.js
@@ -1,16 +1,86 @@
-// array of questions for user
-const questions = [
+const fs = require("fs");
+const util           = require("util");
+const inquirer       = require("inquirer");
+const generateReadme = require("./utils/generateMarkdown")
+const writeFileAsync = util.promisify(fs.writeFile);
 
-];
-
-// function to write README file
-function writeToFile(fileName, data) {
-}
-
-// function to initialize program
-function init() {
-
-}
-
-// function call to initialize program
-init();
+/* Prompt the user questions using inquirer pkg */
+function userCLI(){
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "projectTitle",
+            message: "Enter the PROJECT TITLE > ",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Enter PROJECT DISCRIPTION > "
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Enter PROJECT INSTALLATION process > ",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "Enter PROJECT USAGE > "
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Choose PROJECT DISTRIBUTION LICENSE > ",
+            choices: [
+                "Apache",
+                "Academic",
+                "GNU",
+                "ISC",
+                "MIT",
+                "Mozilla",
+                "Open"
+            ]
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "Enter PROJECT CONTRIBUTORS > "
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "Enter inluded TESTS > "
+        },
+        {
+            type: "input",
+            name: "questions",
+            message: "Enter PROJECT CONTACT information > "
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "Enter your GitHub username > "
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter your email > "
+        }
+    ]);
+} 
+/* Initialize function, asyncronous using promisify */
+async function init() {
+    try {
+        // Ask user questions and generate responses
+        const answers = await userCLI();
+        const generateContent = generateReadme(answers);
+        // Write new README.md to dist directory
+        await writeFileAsync('./output/README.md', generateContent);
+        console.log('✔️  README.md sucessfully generated in ./output/');
+    }   catch(err) {
+        console.log(err);
+    }
+  }
+  
+  /* Launch the README generation */
+  init();  
---
+++ output/README.md
@@ -0,0 +1,41 @@
+
+      <h1 align="center">Test1 👋</h1>
+
+      ![badge](https://img.shields.io/badge/license-MIT-brightgreen)<br />
+
+      ## Description
+      🔍 Testing my README generator
+
+      ## Table of Contents
+      - [Description](#description)
+      - [Installation](#installation)
+      - [Usage](#usage)
+      - [License](#license)
+      - [Contributing](#contributing)
+      - [Tests](#tests)
+      - [Questions](#questions)
+
+      ## Installation
+      💾 npm install inquirer
+
+      ## Usage
+      💻 Test README generation
+
+      ## License
+      ![badge](https://img.shields.io/badge/license-MIT-brightgreen)
+      <br />
+      This application is covered by the MIT license. 
+
+      ## Contributing
+      👪 Sparsh Patwa
+
+      ## Tests
+      ✏️ None
+
+      ## Questions
+      ✋ None<br />
+      <br />
+      :octocat: Find me on GitHub: [sparshpatwa](https://github.com/sparshpatwa)<br />
+      <br />
+      ✉️ Email me with any questions: patwa.sparsh@gmail.com<br /><br />
+  
